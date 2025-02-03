958ead36be435fdb10b0ff97a206413dd663cf25
Komornik Antal <55227338+antal-komornik@users.noreply.github.com>
2025-02-03 19:07:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import FoodLister from "@/components/ui/body/FoodLister";
 import PremiumFloatingButton from "@/components/ui/PremiumFloatingButton";
 import { useSession } from "next-auth/react";
+import { CategoryButton } from '@/components/ui/sidenav/CategoryBtn';
 
 export default function Home() {
   const { data: session } = useSession()
@@ -13,7 +14,26 @@ export default function Home() {
 
       <div className="block md:hidden">
         {/* <h1 className="text-xl font-bold">Mobil Dashboard</h1> */}
-        <FoodLister />
+              <div className="flex px-4 py-2   overflow-x-auto scrollbar-hide z-0 pt-20">
+                <div className='flex h-8 shrink-0 items-center justify-center z-0'>
+                    <CategoryButton
+                    // onCategorySelect={handleCategorySelect}
+                    // activeCategory={activeCategory}
+                    />
+                </div>
+            </div>
+        
+            <div className="flex-1 z-0">
+                <div className="flex  w-full justify-center items-center">
+                    {/* <MainContent
+                        contentRef={contentRef}
+                        categoryRefs={categoryRefs}
+                        activeCategory={activeCategory}
+                    /> */}
+                    <FoodLister />
+                </div>
+            </div>
+{/*         <FoodLister /> */}
 
       </div>
 
