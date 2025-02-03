f84af914e1f6f0391dfc786675f0288420faf7ae
Komornik Antal <55227338+antal-komornik@users.noreply.github.com>
2025-02-03 19:09:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <div className="block md:hidden">
         {/* <h1 className="text-xl font-bold">Mobil Dashboard</h1> */}
-              <div className="flex px-4 py-2   overflow-x-auto scrollbar-hide z-0 pt-20">
+              <div className="flex px-4 py-2   overflow-x-auto scrollbar-hide z-0 ">
                 <div className='flex h-8 shrink-0 items-center justify-center z-0'>
                     <CategoryButton
                     // onCategorySelect={handleCategorySelect}
